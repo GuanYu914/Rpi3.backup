c110deb63273773394cf2b695f9f56df24866103
GuanYu914 <aa774281@gmail.com>
2018-07-31 17:39:33 +0800

    make some configurations for myself.

--- backup.sh
+++ backup.sh
@@ -1,76 +1,35 @@
 #!/bin/bash
 
-# ___ CREDITS
-# This script started from 
-#   http://raspberrypi.stackexchange.com/questions/5427/can-a-raspberry-pi-be-used-to-create-a-backup-of-itself
-# which in turn started from 
-#   http://www.raspberrypi.org/phpBB3/viewtopic.php?p=136912 
-#
-# Users of this script can just modify the below marked values (stopService,startservice function and directory to
-# store the backup
-#
-# 2013-Sept-04
-# Merged in later comments from the original thread (the pv exists check modified) and added the backup.log
-#
-# 2013-Sept-05
-# Remved tar compression, since it takes FOREVER to complete and I don't need it.
-#
-# 2014-Feb-12
-# Merged in comments from http://www.raspberrypi.org/forum/viewtopic.php?f=63&t=12079&p=494488
-# Moved interesting variables to top of script
-# Added options around updates and gzips
-#
-# Add an entry to crontab to run regurlarly.
-# Example: Update /etc/crontab to run backup.sh as root every night at 3am
-# 01 4    * * *   root    /home/pi/scripts/backup.sh
-
-
 # ======================== CHANGE THESE VALUES ========================
 function stopServices {
 	echo -e "${purple}${bold}Stopping services before backup${NC}${normal}" | tee -a $DIR/backup.log
-    sudo service sendmail stop
-    sudo service cron stop
-    sudo service ssh stop
-    sudo pkill deluged
-    sudo pkill deluge-web
-    sudo service deluge-daemon stop
-    sudo ervice btsync stop
-    sudo service apache2 stop
-    sudo service samba stop
-    
-    #sudo service noip stop
-    #sudo service proftpd stop
-    #sudo service webmin stop
-    #sudo service xrdp stop
-    #sudo service ddclient stop
-    #sudo service apache2 stop
-    #sudo service samba stop
-    #sudo service avahi-daemon stop
-    #sudo service netatalk stop
+    	sudo service cron stop
+    	sudo service ssh stop
+    	sudo service apache2 stop
+    	sudo service smbd stop
+    	sudo service nmbd stop
 }
 
 function startServices {
 	echo -e "${purple}${bold}Starting the stopped services${NC}${normal}" | tee -a $DIR/backup.log
-    sudo ervice samba start
-    sudo service apache2 start
-    sudo service btsync start
-    sudo service deluge-daemon start
-    sudo service ssh start
-    sudo service cron start
-    sudo service sendmail start
+	sudo service cron start
+	sudo service ssh start
+	sudo service apache2 start
+	sudo service smbd start
+	sudo service nmbd start
 }
-
 
 # Setting up directories
 SUBDIR=pi-backup
 MOUNTPOINT=/media/usb-drive
-DIR=$MOUNTPOINT/$SUBDIR
-RETENTIONPERIOD=0 # don't keep old backup file
-POSTPROCESS=0 # 1 to use a postProcessSucess function after successfull backup
-GZIP=0 # whether to gzip the backup or not
+DIR=$MOUNTPOINT/$SUBDIR	
+RETENTIONPERIOD=0 		# don't keep old backup file
+POSTPROCESS=1 			# 1 to use a postProcessSucess function after successfull backup
+GZIP=0 				# whether to gzip the backup or not
 
 # Function which tries to mount MOUNTPOINT
-function mountMountPoint {
+function mountMountPoint 
+{
     # mount all drives in fstab (that means MOUNTPOINT needs an entry there)
     mount -a
 }
@@ -79,23 +38,22 @@ function mountMountPoint {
 function postProcessSucess {
 	# Update Packages and Kernel
 	echo -e "${yellow}Update Packages and Kernel${NC}${normal}" | tee -a $DIR/backup.log
-    sudo apt-get update
-    sudo apt-get upgrade -y
-    sudo apt-get autoclean
+    	sudo apt-get update
+    	sudo apt-get upgrade -y
+    	sudo apt-get autoclean
 
-    echo -e "${yellow}Update Raspberry Pi Firmware${NC}${normal}" | tee -a $DIR/backup.log
-    sudo rpi-update
-    sudo ldconfig
+    	#echo -e "${yellow}Update Raspberry Pi Firmware${NC}${normal}" | tee -a $DIR/backup.log
+    	#sudo rpi-update
+    	#sudo ldconfig
 
-    # Reboot now
-    echo -e "${yellow}Reboot now ...${NC}${normal}" | tee -a $DIR/backup.log
-    sudo reboot
+    	# Reboot now
+    	#echo -e "${yellow}Reboot now ...${NC}${normal}" | tee -a $DIR/backup.log
+    	#sudo reboot
 }
 
 # =====================================================================
 
-
-# Setting up echo fonts
+# setting up echo fonts
 red='\e[0;31m'
 green='\e[0;32m'
 cyan='\e[0;36m'
@@ -145,7 +103,7 @@ fi
 
 # Remove previous backup img files
 cd $DIR
-sudo /bin/rm *
+sudo /bin/rm *.img
 echo -e "${yellow}Remove previous image file.....done!" 
 
 # Create a filename with datestamp for our current backup
